a1cdc82ce382e2c44e4c24885c7ee0d2cb243219
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-07 19:54:49 +0600

    [Bug fixed] : Bug fix for display album items

--- script.js
+++ script.js
@@ -40,28 +40,28 @@ const showArtists = (data) => {
 };
 
 const fetchAlbums = (id) => {
-  const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
+  const url = `https://theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
   fetch(url)
-    .then((res) => res.JSON())
-    .then((data) => showAlbum(data));
+    .then((res) => res.json())
+    .then((data) => showAlbum(data.album));
   const artistContainer = elementById("artists");
   artistContainer.innerHTML = "";
 };
 
-const showAlbum = (data) => {
+const showAlbum = (items) => {
   const albumContainer = elementById("albums");
-  album.forEach((item) => {
+  items.forEach((item) => {
     const div = document.createElement("div");
     div.classList.add("album");
     div.innerHTML = `
         <div class="album-image-container">
           <img
-            src="${album.strAlbumThumb}"
+            src="${item.strAlbumThumb}"
             alt=""
           />
         </div>
         <div class="album-name">
-          <h3>${album.strAlbum}</h3>
+          <h3>${item.strAlbum}</h3>
         </div>
       `;
 
